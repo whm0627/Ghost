e967ef7284241117bb645ce39aab999a5eae0d54
Heming Wang <heming.wang@yahoo.com>
2017-01-01 23:14:59 -0800

    port

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 
 set
+erb core/server/config/env/config.production.json.erb
 erb core/server/config/env/config.production.json.erb > core/server/config/env/config.production.json
 cat core/server/config/env/config.production.json
 npm start --production
